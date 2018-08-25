3799684001251cae5af974ae6fe14f7d390a4a43
sgiehl <stefan@piwik.org>
2018-08-25 13:57:53 +0200

    Converts UI tests to use Chrome Headless

--- tests/UI/CustomDimensions_spec.js
+++ tests/UI/CustomDimensions_spec.js
@@ -15,36 +15,41 @@ describe("CustomDimensions", function () {
     var generalParams = 'idSite=1&period=year&date=2013-01-23',
         urlBase = 'module=CoreHome&action=index&' + generalParams;
 
-    var manageUrl = "?" + generalParams + "&module=CustomDimensions&action=manage";
     var reportUrl = "?" + urlBase + "#?" + generalParams;
 
     var reportUrlDimension2 = reportUrl + "&category=General_Visitors&subcategory=customdimension2";
     var reportUrlDimension3 = reportUrl + "&category=General_Actions&subcategory=customdimension3";
     var reportUrlDimension4 = reportUrl + "&category=General_Actions&subcategory=customdimension4";
 
-    var popupSelector = '.pageWrap,.ui-dialog:visible';
+    var popupSelector = '.ui-dialog:visible';
 
-    function capturePageWrap (screenName, callback, done) {
-        expect.screenshot(screenName).to.be.captureSelector('.pageWrap', callback, done);
+    async function capturePageWrap (screenName, test) {
+        await test();
+        var elem = await page.jQuery('.pageWrap');
+        expect(await elem.screenshot()).to.matchImage(screenName);
     }
 
-    function captureSelector (screenName, selector, callback, done) {
-        expect.screenshot(screenName).to.be.captureSelector(selector, callback, done);
+    async function captureSelector (screenName, selector, test) {
+        await test();
+        var elem = await page.jQuery(selector);
+        expect(await elem.screenshot()).to.matchImage(screenName);
     }
 
-    function closeOpenedPopover(page)
+    async function closeOpenedPopover()
     {
-        page.click('.ui-dialog:visible .ui-button-icon-primary.ui-icon-closethick:visible');
+        await (await page.jQuery('.ui-dialog:visible .ui-button-icon-primary.ui-icon-closethick:visible')).click();
     }
 
-    function triggerRowAction(page, labelToClick, nameOfRowActionToTrigger)
+    async function triggerRowAction(labelToClick, nameOfRowActionToTrigger)
     {
         var rowToMatch = 'td.label:contains(' + labelToClick + '):first';
 
-        page.mouseMove('table.dataTable tbody ' + rowToMatch);
-        page.mouseMove(rowToMatch + ' a.'+ nameOfRowActionToTrigger + ':visible'); // necessary to get popover to display
-        page.mouseMove(rowToMatch + ' a.'+ nameOfRowActionToTrigger + ':visible'); // necessary to get popover to display
-        page.click(rowToMatch + ' a.' + nameOfRowActionToTrigger + ':visible', 1500);
+        await (await page.jQuery('table.dataTable tbody ' + rowToMatch)).hover();
+        await (await page.jQuery(rowToMatch + ' a.'+ nameOfRowActionToTrigger + ':visible')).hover(); // necessary to get popover to display
+        await (await page.jQuery(rowToMatch + ' a.' + nameOfRowActionToTrigger + ':visible')).click();
+        await page.mouse.move(-10, -10);
+        await page.waitFor(250); // wait for animation
+        await page.waitForNetworkIdle();
     }
 
     before(function () {
@@ -56,100 +61,104 @@ describe("CustomDimensions", function () {
      * VISIT DIMENSION REPORTS
      */
 
-    it('should show the report for the selected visit dimension', function (done) {
-        capturePageWrap('report_visit', function (page) {
-            page.load(reportUrlDimension2);
-        }, done);
+    it('should show the report for the selected visit dimension', async function () {
+        await capturePageWrap('report_visit', async function () {
+            await page.goto(reportUrlDimension2);
+        });
     });
 
-    it('should add a menu item for each active visit dimension', function (done) {
-        captureSelector('report_visit_mainmenu', '#secondNavBar', function (page) {
+    it('should add a menu item for each active visit dimension', async function () {
+        await captureSelector('report_visit_mainmenu', '#secondNavBar', async function () {
             // we only capture a screenshot of a different part of the page, no need to do anything
-        }, done);
+        });
     });
 
-    it('should add visit dimensions to goals report', function (done) {
-        captureSelector('report_goals_overview', '.reportsByDimensionView', function (page) {
-            page.load( "?" + urlBase + "#?" + generalParams + "&category=Goals_Goals&subcategory=General_Overview");
-            page.click('.reportsByDimensionView .dimension:contains(MyName1)');
-        }, done);
+    it('should add visit dimensions to goals report', async function () {
+        await captureSelector('report_goals_overview', '.reportsByDimensionView', async function () {
+            await page.goto( "?" + urlBase + "#?" + generalParams + "&category=Goals_Goals&subcategory=General_Overview");
+            await (await page.jQuery('.reportsByDimensionView .dimension:contains(MyName1)')).click();
+            await page.waitForNetworkIdle();
+        });
     });
 
     /**
      * ACTION DIMENSION REPORTS
      */
 
-    it('should show the report for the selected action dimension', function (done) {
-        capturePageWrap('report_action', function (page) {
-            page.load(reportUrlDimension3);
-        }, done);
+    it('should show the report for the selected action dimension', async function () {
+        await capturePageWrap('report_action', async function () {
+            await page.goto(reportUrlDimension3);
+        });
     });
 
-    it('should add a menu item for each active action dimension', function (done) {
-        captureSelector('report_actions_mainmenu', '#secondNavBar', function (page) {
+    it('should add a menu item for each active action dimension', async function () {
+        await captureSelector('report_actions_mainmenu', '#secondNavBar', async function () {
             // we only capture a screenshot of a different part of the page, no need to do anything
-        }, done);
+        });
     });
 
-    it('should offer only segmented visitor log and row action for first level entries', function (done) {
-        capturePageWrap('report_actions_rowactions', function (page) {
-            page.mouseMove('td.label:contains(en):first');
-        }, done);
+    it('should offer only segmented visitor log and row action for first level entries', async function () {
+        await capturePageWrap('report_actions_rowactions', async function () {
+            await (await page.jQuery('td.label:contains(en):first')).hover();
+        });
     });
 
-    it('should be able to render insights', function (done) {
-        capturePageWrap('report_action_insights', function (page) {
-            page.click('[data-footer-icon-id="insightsVisualization"]');
-        }, done);
+    it('should be able to render insights', async function () {
+        await capturePageWrap('report_action_insights', async function () {
+            await page.evaluate(function(){
+                $('[data-footer-icon-id="insightsVisualization"]').click();
+            });
+            await page.waitForNetworkIdle();
+        });
     });
 
-    it('should show an error when trying to open an inactive dimension', function (done) {
-        expect.page("").contains(".pageWrap:contains('This page does not exist')", function (page) {
-            page.load(reportUrlDimension4);
-        }, done);
+    it('should show an error when trying to open an inactive dimension', async function () {
+        await page.goto(reportUrlDimension4);
+        await page.waitForFunction('$(".pageWrap:contains(\'This page does not exist\')").length > 0');
     });
 
-    it('should be able to open segmented visitor log', function (done) {
-        captureSelector('report_actions_segmented_visitorlog', popupSelector, function (page) {
-            page.load(reportUrlDimension3);
-            triggerRowAction(page, 'en', 'actionSegmentVisitorLog');
-        }, done);
+    it('should be able to open segmented visitor log', async function () {
+        await captureSelector('report_actions_segmented_visitorlog', popupSelector, async function () {
+            await page.goto(reportUrlDimension3);
+            await triggerRowAction('en', 'actionSegmentVisitorLog');
+        });
     });
 
-    it('should be able to open row evolution', function (done) {
-        captureSelector('report_actions_rowevolution', popupSelector, function (page) {
-            page.load(reportUrlDimension3);
-            triggerRowAction(page, 'en', 'actionRowEvolution');
-        }, done);
+    it('should be able to open row evolution', async function () {
+        await captureSelector('report_actions_rowevolution', popupSelector, async function () {
+            await page.goto(reportUrlDimension3);
+            await triggerRowAction('en', 'actionRowEvolution');
+        });
     });
 
-    it('should be able to show subtable and offer all row actions if scope is action', function (done) {
-        capturePageWrap('report_action_subtable', function (page) {
-            page.load(reportUrlDimension3);
-            page.click('.dataTable .subDataTable .value:contains(en):first');
-            page.mouseMove('td.label:contains(en_US)');
-        }, done);
+    it('should be able to show subtable and offer all row actions if scope is action', async function () {
+        await capturePageWrap('report_action_subtable', async function () {
+            await page.goto(reportUrlDimension3);
+            await (await page.jQuery('.dataTable .subDataTable .value:contains(en):first')).click();
+            await page.waitForNetworkIdle();
+            await (await page.jQuery('td.label:contains(en_US)')).hover();
+            await page.waitFor(100);
+        });
     });
 
-    it('should be able to show row evolution for subtable', function (done) {
-        captureSelector('report_action_subtable_rowevolution', popupSelector, function (page) {
-            triggerRowAction(page, 'en_US', 'actionRowEvolution');
-        }, done);
+    it('should be able to show row evolution for subtable', async function () {
+        await captureSelector('report_action_subtable_rowevolution', popupSelector, async function () {
+            await triggerRowAction('en_US', 'actionRowEvolution');
+        });
     });
 
-    it('should be able to show segmented visitor log for subtable', function (done) {
-        captureSelector('report_action_subtable_segmented_visitor_log', popupSelector, function (page) {
-            closeOpenedPopover(page);
-            triggerRowAction(page, 'en_US', 'actionSegmentVisitorLog');
-        }, done);
+    it('should be able to show segmented visitor log for subtable', async function () {
+        await captureSelector('report_action_subtable_segmented_visitor_log', popupSelector, async function () {
+            await closeOpenedPopover();
+            await triggerRowAction('en_US', 'actionSegmentVisitorLog');
+        });
     });
 
-    it('should be able to show transitions for subtable', function (done) {
-        capturePageWrap('report_action_subtable_transitions', function (page) {
-            closeOpenedPopover(page);
-            triggerRowAction(page, 'en_US', 'actionTransitions');
-        }, done);
+    it('should be able to show transitions for subtable', async function () {
+        await captureSelector('report_action_subtable_transitions', popupSelector, async function () {
+            await closeOpenedPopover();
+            await triggerRowAction('en_US', 'actionTransitions');
+        });
     });
-
 
 });
